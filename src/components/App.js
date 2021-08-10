cf629b228ba7b5630ffd2a781032634cc27ed1f1
aryanmajeed <aryanmajid97@gmail.com>
2021-08-10 22:49:59 +0300

    Done.

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import Navbar from "./Navbar";
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <Switch>
+                <Route exact path="/about">
+                    <About />
+                </Route>
+                <Route exact path="/login">
+                    <Login />
+                </Route>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="*">
+                    <p>not found</p>
+                </Route>
+            </Switch>
+        </div>
+    );
+}
+
+export default App;
